2e4f66c90ab72c57a9ea3102d71612e6cde47dae
youfoundmalik <godswill.ugochukwu@fcmb.com>
2025-03-09 02:32:15 +0100

    search behavior

--- src/components/JobFilters.tsx
+++ src/components/JobFilters.tsx
@@ -8,8 +8,6 @@ import { useJobsContext } from "@/hooks/useJobContext";
 import { jobTypes } from "@/utils/constants";
 import { JobFilterParams } from "@/types/job";
 
-let timeout: NodeJS.Timeout;
-
 export default function JobFilters({ isLoading, params }: { isLoading: boolean; params: JobFilterParams }) {
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -20,33 +18,51 @@ export default function JobFilters({ isLoading, params }: { isLoading: boolean; 
     skills: params.skills?.[0] ?? "",
   });
 
-  const handleSearch = (key: string, value: string) => {
-    clearTimeout(timeout);
-    setValues((p) => ({ ...p, [key]: value }));
-
+  const handleSearch = (payload = values) => {
     const params = new URLSearchParams(searchParams.toString());
 
-    if (value) {
-      params.set(key, value);
+    if (payload.location) {
+      params.set("location", payload.location);
     } else {
-      params.delete(key);
+      params.delete("location");
     }
 
-    timeout = setTimeout(() => {
-      setIsLoading(true);
-      router.push(`/?${params.toString()}`);
-    }, 500);
+    if (payload.job_type) {
+      params.set("job_type", payload.job_type);
+    } else {
+      params.delete("job_type");
+    }
+
+    if (payload.skills) {
+      params.set("skills", payload.skills);
+    } else {
+      params.delete("skills");
+    }
+
+    setIsLoading(true);
+    router.push(`/?${params.toString()}`);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleSearch();
   };
 
   return (
-    <div className='flex items-center gap-2 md:gap-6 flex-col md:flex-row'>
+    <form onSubmit={handleSubmit} className='flex items-center gap-2 md:gap-6 flex-col md:flex-row'>
       <Input
         value={values.location}
         containerClass='w-full md:w-[218px]'
         className='placeholder:text-foreground text-sm'
         placeholder='Location'
         disabled={isLoading}
-        onChange={(e) => handleSearch("location", e.target.value)}
+        onBlur={() => handleSearch()}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleSearch();
+          }
+        }}
+        onChange={(e) => setValues((p) => ({ ...p, location: e.target.value }))}
       />
       <Select
         value={values.job_type}
@@ -54,7 +70,11 @@ export default function JobFilters({ isLoading, params }: { isLoading: boolean; 
         containerClass='w-full md:w-[218px]'
         placeholder='All'
         disabled={isLoading}
-        onChange={(e) => handleSearch("job_type", e.target.value)}
+        onChange={(e) => {
+          const payload = { ...values, job_type: e.target.value };
+          setValues(payload);
+          handleSearch(payload);
+        }}
       />
       <Input
         value={values.skills}
@@ -62,8 +82,14 @@ export default function JobFilters({ isLoading, params }: { isLoading: boolean; 
         className='placeholder:text-foreground text-sm'
         placeholder='Skills'
         disabled={isLoading}
-        onChange={(e) => handleSearch("skills", e.target.value)}
+        onBlur={() => handleSearch()}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleSearch();
+          }
+        }}
+        onChange={(e) => setValues((p) => ({ ...p, skills: e.target.value }))}
       />
-    </div>
+    </form>
   );
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,17 +5,13 @@ import { AddIcon, Button, Input, SearchIcon } from "../common";
 import { useSearchParams, useRouter } from "next/navigation";
 import { useJobsContext } from "@/hooks/useJobContext";
 
-let timeout: NodeJS.Timeout;
-
 const Header = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const { setIsLoading } = useJobsContext();
   const [value, setValue] = useState(searchParams.get("search_term") ?? "");
 
-  const handleSearch = (value: string) => {
-    clearTimeout(timeout);
-    setValue(value);
+  const handleSearch = () => {
     const params = new URLSearchParams(searchParams.toString());
 
     if (value) {
@@ -24,23 +20,34 @@ const Header = () => {
       params.delete("search_term");
     }
 
-    timeout = setTimeout(() => {
-      setIsLoading(true);
-      router.push(`/?${params.toString()}`);
-      return () => clearTimeout(timeout);
-    }, 500);
+    setIsLoading(true);
+
+    router.push(`/?${params.toString()}`);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleSearch();
   };
 
   return (
     <div className='md:px-8 pb-4 pt-6 flex items-center justify-between border-b border-base-gray-100'>
-      <Input
-        icon={<SearchIcon />}
-        value={value}
-        onChange={(e) => handleSearch(e.target.value)}
-        containerClass='w-full md:w-[340px] !rounded-full !bg-base-gray-200'
-        className='text-sm placeholder:text-base-gray-800'
-        placeholder='Search for jobs'
-      />
+      <form onSubmit={handleSubmit}>
+        <Input
+          icon={<SearchIcon />}
+          value={value}
+          onBlur={() => handleSearch()}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSearch();
+            }
+          }}
+          onChange={(e) => setValue(e.target.value)}
+          containerClass='w-full md:w-[340px] !rounded-full !bg-base-gray-200'
+          className='text-sm placeholder:text-base-gray-800'
+          placeholder='Search for jobs'
+        />
+      </form>
       <Button className='bg-primary text-white rounded-full hover:opacity-90 transition-opacity duration-100 hidden md:flex' icon={<AddIcon />}>
         <span className='pr-1'>Create New Job Post</span>
       </Button>
